c9240b0c92673564ea6e95cce68cbb792fc3033a
Nguyen Duc Tri <nguyenductri2k3@gmail.com>
2024-06-25 22:34:06 +0700

    Switch to YAML file, BunJs, remove .git folder

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 require("dotenv").config();
 
-const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const fs = require("fs");
+const YAML = require("yaml");
 
-const indexRouter = require("./routes/index");
 const app = express();
 
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+const file = fs.readFileSync("./swagger/build.yaml", "utf8");
+const swaggerDocument = YAML.parse(file);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -22,23 +23,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-// error handler
-app.use((err, req, res, _) => {
-  // set locals, only providing error in development
-  res["locals"].message = err["message"];
-  res["locals"].error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res["status"](err["status"] || 500);
-  res["render"]("error");
-});
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
